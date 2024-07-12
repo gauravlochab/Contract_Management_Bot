540d60d700af5b2e75b39f7a473dbf3212f111f9
Gaurav Lochab <gauravlochab1997@gmail.com>
2024-07-12 21:02:53 +0530

    Update retrieval_flask.py

--- chatbot_qa/retrieval/retrieval_flask.py
+++ chatbot_qa/retrieval/retrieval_flask.py
@@ -15,4 +15,4 @@ def api_get_response():
     return jsonify({'response': response})
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8001)
+    app.run(debug=True, host='0.0.0.0', port=8002)
